e313d04489295a54abd84dc5034074d55a8ad565
mxro <mxro@nowhere.com>
2014-08-11 14:13:22 +1000

    Improving persistence API Autocommit Mon Aug 11 14:13:22 EST 2014

--- src/main/java/de/mxro/fn/Closure.java
+++ src/main/java/de/mxro/fn/Closure.java
@@ -1,7 +1,19 @@
 package de.mxro.fn;
 
+/**
+ * <p>A simple closure which takes in one generic argument.
+ * 
+ * @author Max
+ *
+ * @param <P> The type of the argument for the closure.
+ */
 public interface Closure<P> {
 
+	/**
+	 * The method of this closure.
+	 * 
+	 * @param o The parameter for the closure.
+	 */
 	public void apply(P o);
 
 }
--- src/main/java/de/mxro/fn/Closure2.java
+++ src/main/java/de/mxro/fn/Closure2.java
@@ -1,7 +1,23 @@
 package de.mxro.fn;
 
+/**
+ * A closure which takes in two generic arguments.
+ * 
+ * @author Max
+ *
+ * @param <P1>
+ *            Type of the first argument.
+ * @param <P2>
+ *            Type of the second argument.
+ */
 public interface Closure2<P1, P2> {
 
+	/**
+	 * The main method of this closure.
+	 * 
+	 * @param p1 First parameter to be passed.
+	 * @param p2 Second parameter to be passed.
+	 */
 	public void apply(P1 p1, P2 p2);
-	
+
 }
