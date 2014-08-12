52abccf4d7d57dd098a7651af824d780ed8a12a4
mxro <mxro@nowhere.com>
2014-08-12 11:57:16 +1000

    Reworking Nextweb persistence connection and implementing asyc-map framework Autocommit Tue Aug 12 11:57:16 EST 2014

--- src/main/java/de/mxro/fn/Fn.java
+++ src/main/java/de/mxro/fn/Fn.java
@@ -2,10 +2,21 @@ package de.mxro.fn;
 
 public class Fn {
 
-	public Object object() {
+	/**
+	 * <p>Creates an object with a unique identity. The object will only be equal
+	 * to itself.
+	 * 
+	 * @return An object with a unique identity.
+	 */
+	public static final Object object() {
 		return new Object() {
+
+			@Override
+			public boolean equals(Object obj) {
+				return obj == this;
+			}
 			
 		};
 	}
-	
+
 }
