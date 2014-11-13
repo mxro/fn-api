079acbf2cbecb2cf2f2d41e23c0b8769934fbbbc
mxro <mxro@nowhere.com>
2014-11-14 10:33:19 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 10:33:19 EST 2014

--- src/main/java/de/mxro/fn/collections/CollectionsUtils.java
+++ src/main/java/de/mxro/fn/collections/CollectionsUtils.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public final class CollectionsUtils {
 
-    public static <GPOutput> boolean isMapComplete(final Map<Integer, GPOutput> map, final int size) {
+    public final static <GPOutput> boolean isMapComplete(final Map<Integer, GPOutput> map, final int size) {
         for (int i = 0; i < size; i++) {
             if (map.get(Integer.valueOf(i)) == null) {
                 return false;
@@ -15,7 +15,7 @@ public final class CollectionsUtils {
         return true;
     }
 
-    public static <GOutput> List<GOutput> toOrderedList(final Map<Integer, GOutput> responseMap) {
+    public final static <GOutput> List<GOutput> toOrderedList(final Map<Integer, GOutput> responseMap) {
         final List<GOutput> localResponses = new ArrayList<GOutput>(responseMap.size());
         for (int i = 0; i < responseMap.size(); i++) {
             final GOutput rr = responseMap.get(Integer.valueOf(i));
@@ -25,6 +25,16 @@ public final class CollectionsUtils {
             localResponses.add(rr);
         }
         return localResponses;
+    }
+
+    /**
+     * Flattens the provided list into a single list.
+     * 
+     * @param lists
+     * @return
+     */
+    public final static <V> List<V> flatten(final List<List<V>> lists) {
+
     }
 
 }
