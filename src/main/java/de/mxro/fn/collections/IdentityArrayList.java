488d9734a2e2bef8b20f34753a5c8076af6f9257
mxro <mxro@nowhere.com>
2014-11-18 08:58:40 +1100

    Adding support for more file types Autocommit Tue Nov 18 08:58:40 EST 2014

--- src/main/java/de/mxro/fn/collections/IdentityArrayList.java
+++ src/main/java/de/mxro/fn/collections/IdentityArrayList.java
@@ -32,44 +32,52 @@ import java.util.Collection;
  */
 public class IdentityArrayList<E> extends ArrayList<E> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public boolean remove(final Object o) {
-		return super.remove(this.indexOf(o)) != null;
-	}
+    @Override
+    public boolean remove(final Object o) {
+        final int indexOf = this.indexOf(o);
+        if (indexOf == -1) {
+            return false;
+        }
+        return super.remove(indexOf) != null;
+    }
 
-	@Override
-	public boolean contains(final Object o) {
-		return indexOf(o) >= 0;
-	}
+    @Override
+    public boolean contains(final Object o) {
+        return indexOf(o) >= 0;
+    }
 
-	@Override
-	public int indexOf(final Object o) {
-		for (int i = 0; i < size(); i++)
-			if (o == get(i))
-				return i;
-		return -1;
-	}
+    @Override
+    public int indexOf(final Object o) {
+        for (int i = 0; i < size(); i++) {
+            if (o == get(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	@Override
-	public int lastIndexOf(final Object o) {
-		for (int i = size() - 1; i >= 0; i--)
-			if (o == get(i))
-				return i;
-		return -1;
-	}
+    @Override
+    public int lastIndexOf(final Object o) {
+        for (int i = size() - 1; i >= 0; i--) {
+            if (o == get(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public IdentityArrayList() {
-		super();
-	}
+    public IdentityArrayList() {
+        super();
+    }
 
-	public IdentityArrayList(final Collection<? extends E> c) {
-		super(c);
-	}
+    public IdentityArrayList(final Collection<? extends E> c) {
+        super(c);
+    }
 
-	public IdentityArrayList(final int initialCapacity) {
-		super(initialCapacity);
-	}
+    public IdentityArrayList(final int initialCapacity) {
+        super(initialCapacity);
+    }
 
 }
