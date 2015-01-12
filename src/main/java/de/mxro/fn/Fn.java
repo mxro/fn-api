a68679c524b970d38015ad9c8a413510185b70d5
mxro <mxro@nowhere.com>
2015-01-13 10:16:26 +1100

    Fixing session exceptions Autocommit Tue Jan 13 10:16:26 EST 2015

--- src/main/java/de/mxro/fn/Fn.java
+++ src/main/java/de/mxro/fn/Fn.java
@@ -2,21 +2,32 @@ package de.mxro.fn;
 
 public class Fn {
 
-	/**
-	 * <p>Creates an object with a unique identity. The object will only be equal
-	 * to itself.
-	 * 
-	 * @return An object with a unique identity.
-	 */
-	public static final Object object() {
-		return new Object() {
+    /**
+     * <p>
+     * Creates an object with a unique identity. The object will only be equal
+     * to itself.
+     * 
+     * @return An object with a unique identity.
+     */
+    public static final Object object() {
+        return new Object() {
 
-			@Override
-			public boolean equals(Object obj) {
-				return obj == this;
-			}
-			
-		};
-	}
+            @Override
+            public boolean equals(final Object obj) {
+                return obj == this;
+            }
+
+        };
+    }
+
+    public static Closure<Success> expectSuccess() {
+        return new Closure<Success>() {
+
+            @Override
+            public void apply(final Success o) {
+
+            }
+        };
+    }
 
 }
